43998c9268b8158a37b9b59ee8c45b2a63511498
sagardafle <sagar.cdafle@gmail.com>
2017-04-03 02:42:12 -0700

    Final merge: Code to get the required distance and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# dronedeply-coding-challenge
+# Dronedeply-coding-challenge
+
+## Challenge-1
+
+
+This zip file https://www.dropbox.com/s/afrfye7hvra7wvy/images.zip?dl=0 contains a number of images taken from different positions and orientations with an iPhone 6. Each image is the view of a pattern on a flat surface. The original pattern that was photographed is 8.8cm x 8.8cm and is included in the zip file. Write a Python program that will visualize (i.e. generate a graphic) where the camera was when each image was taken and how it was posed, relative to the pattern.
+
+You can assume that the pattern is at 0,0,0 in some global coordinate system and are thus looking for the x, y, z and yaw, pitch, roll of the camera that took each image. Please submit a link to a Github repository contain the code for your solution. Readability and comments are taken into account too. You may use 3rd party libraries like OpenCV and Numpy.
+## Approach:
+
+1. Installed Python,OpenCV and the Numpy libraries.
+2. Below are the steps involved in solving the coding challenge:
+
+ - Read any of the input images using cv2. I chose (IMG_6726.jpg).
+ - Later ,we scale the image to 600*800 size. We finalize on the specifications by trial methods.
+ - Convert input image to gray scale.
+ - Thresholding an Image using THRESH_BINARY to classify image into white and black.
+ - Contouring an image.
+ - Detecting and analysing only the barcode pattern from the entire white paper.
+ - Analyse the rotation of the pattern.
+ - Get the rotated angle of the pattern.
+ - Get the the distance between contour corner and bounded rectangle.
+
+ Each of these steps are heavily commented in the "visualize.py" file along with the usage of inbuilt CV library functions.
+
+ Please note that this repository is PRIVATE as of now and I have only shared it with authorized github users.
+ Let me know if the access is to be given to any other user(s) or if the repo has to be made public.
+
+
+
+
+
--- visualize.py
+++ visualize.py
@@ -1,9 +1,12 @@
+import math
+
 import cv2
 import numpy as np
 
 # STEP-1: Read any of the input images using cv2
-img = cv2.imread('images/IMG_6726.jpg')
-
+imgpath = 'images/IMG_6726.jpg'
+img = cv2.imread(imgpath)
+print("Processing image: ", imgpath)
 # Step-2: We scale the image to 600*800 size. We finalize on the specifications by trial methods.
 img = cv2.resize(img, (600, 800))
 
@@ -33,10 +36,10 @@ cv2.waitKey()
 
 # Let's compute the area for each contour co-ordinates.
 areas = [cv2.contourArea(contour) for contour in contours]
-print (areas)
+# print (areas)
 # #Index of the contour with max area.
 max_area_index = np.argmax(areas)
-print ("max_area_index", max_area_index)
+# print ("max_area_index", max_area_index)
 
 # #Get the contour array(The one that represents the pattern in the white image)
 patterncontour = contours[max_area_index]
@@ -76,7 +79,7 @@ else:
 # =============================Step-8:  Get the rotated angle of the pattern.=============#
 # minAreaRect : gets the  minimum-area bounding the rotated rectangle for a specified contour
 rect = cv2.minAreaRect(patterncontour)
-print (rect)
+# print (rect)
 # boxPoints: Helps us finding the four vertices of a rotated rect.
 box = cv2.boxPoints(rect)
 
@@ -86,4 +89,76 @@ box = np.int0(box)
 cv2.drawContours(img, [box], 0, (0, 0, 255), 2)
 rotatedangle = rect[2]
 
-print("Rotation Angle of the pattern: ", rotatedangle)
+print("Rotation Angle of the barcode pattern: {0:.2f} ".format(rotatedangle))
+
+
+# =========================Step-9: Get the the distance between contour corner and bounded rectangle. =====================
+
+# Functions to compute the mid point and distance between two points#
+def mid_point(point_X, point_Y):
+    return [(point_X[0] + point_Y[0]) / 2, (point_X[1] + point_Y[1]) / 2]
+
+
+def distance(point_X, point_Y):
+    return math.sqrt((point_X[0] - point_Y[0]) ** 2 + (point_X[1] - point_Y[1]) ** 2)
+
+
+# Let's calculate the amount of skew in the image (represented as min_dist in pixels).
+
+min_dist = 1000
+
+for point in box:
+    temp_dist = distance(patterncontour[0][0], point)
+    min_dist = min(min_dist, temp_dist)
+
+print("Degree from the birds eye view:  {0:.2f} ".format(min_dist))
+
+# A bounded rectangular box obtained in above steps will help us determine the distance between the camera and the barcode.
+# Assigning the box co-ordinates to temp variables.
+P = box[0]
+Q = box[1]
+R = box[2]
+S = box[3]
+
+# Calculate the midpoint between each box point lines.
+P_Q = mid_point(P, Q)
+R_S = mid_point(R, S)
+P_S = mid_point(P, S)
+Q_R = mid_point(Q, R)
+
+# Calculate the distance between each parallel box point lines.
+PS_QR_dist = distance(P_S, Q_R)
+PQ_RS_dist = distance(P_Q, R_S)
+
+if PS_QR_dist > PQ_RS_dist:
+    width = PS_QR_dist
+    height = PS_QR_dist
+else:
+    width = PS_QR_dist
+    height = PS_QR_dist
+
+# at 1 foot away the paper appears 450 by 580 pixels
+# this is needed for a depth ratio
+width_ratio_1foot = 450
+height_ratio_1foot = 580
+# at 2 feet away the paper appears 219 by 285 pixels
+width_ratio_2foot = 219
+height_ratio_2foot = 285
+
+# use scaling factors from 1 foot and 2 feet
+one_foot_height = 1 / (height / height_ratio_1foot)
+one_foot_width = 1 / (width / width_ratio_1foot)
+
+two_foot_height = 2 / (height / height_ratio_2foot)
+two_foot_width = 2 / (width / width_ratio_2foot)
+
+# Final distance is the average of above 4 parameters.
+distance_away = (one_foot_width + one_foot_height + two_foot_width + two_foot_height) / 4
+
+print("Image was taken {0:.2f} feet away ".format(distance_away, min_dist, rotatedangle))
+
+cv2.drawContours(img, contours, max_area_index, (0, 125, 0), 3)
+cv2.imshow("output image", img)
+cv2.imwrite("intermediateoutput/output-image.PNG", img);  # Eriting  the output image.
+cv2.waitKey(0)
+cv2.destroyAllWindows()
